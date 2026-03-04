37471d5f1f877d81e243042e4e0b8cdb613c2ea4
zzzimtzoe <zimtzoezottelzeo@pm.me>
2026-03-04 20:49:42 +0100

    faster Julia Monte Carlo DLA version

---
+++ assignment2/monte_carlo_dla.jl
@@ -0,0 +1,147 @@
+using Random
+using Plots
+
+#=
+Monte Carlo simulation of Diffusion-Limited Aggreration (DLA)
+
+- single seed at bottom of domain
+- release random walkers from upper boundary
+- walker peforms random walk until reaches cluster or walks outside system
+- walker added to cluster if it reaches candidates (4-neighborhood grid)
+- new random walker launched
+
+boundaries:
+- periodic boundary for left and right
+- closed boundary for top and bottom
+
+grid size: 100 x 100
+=#
+
+
+NEIGHBORS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+grid_len = 100
+max_steps = 10000
+max_mass = 400
+seed = 7
+
+
+function launch_point(rng)
+    #= Choose random point on the top boundary as launching point for random walker. =#
+
+    j = rand(rng, 1:grid_len)
+    return (1, j)
+end
+
+
+function construct_neighborhood(cluster)
+    #= Keeps track of the candidates in the neighborhood of the cluster.
+    - candidates are empty sites in the 4-neighborhood of the occupied sites
+    - include periodic and absorbing boundaries to avoid out of bound neighbors =#
+    
+    neighborhood = Set{Tuple{Int, Int}}()
+    for (i, j) in cluster
+        for (di, dj) in NEIGHBORS
+            cand_i = i + di
+            cand_j = ((j - 1 + dj) % grid_len) + 1 #periodic boundary
+            if 0 < cand_i <= grid_len && !in((cand_i, cand_j), cluster)
+                push!(neighborhood, (cand_i, cand_j))
+            end
+        end
+    end
+    return neighborhood
+end
+
+
+function update_neighborhood(neighborhood, added_candidate, cluster)
+    #= If a candidate is added to the cluster, the neighborhood is updated.
+    - added candidate is removed from the neighborhood
+    - its empty neighbors become candidates, which are added to the neighborhood
+    - include periodic and absorbing boundaries to avoid out of bound neighbors =#
+
+    pop!(neighborhood, added_candidate)
+    i, j = added_candidate
+    for (di, dj) in NEIGHBORS
+        cand_i = i + di
+        cand_j = ((j - 1 + dj) % grid_len) + 1 #periodic boundary
+        if 0 < cand_i <= grid_len && !in((cand_i, cand_j), cluster)
+            push!(neighborhood, (cand_i, cand_j))
+        end
+    end
+end
+
+
+function release_random_walker(rng, neighborhood)
+    #= Single random walker performs random walk from launching point.
+    - starts on launching point in top boundary
+    - added to cluster if reaches neighborhood --> new walker
+    - if random walker goes beyond the top and bottom boundary, it is removed --> new walker
+    - if random walker reaches left or right boundary, it keeps going on the other side (periodic boundary)
+    - if max steps reached, give up walker --> new walker =#
+
+    i, j = launch_point(rng)
+
+    for _ in 1:max_steps
+        # check if walker is on site in neighborhood
+        if in((i, j), neighborhood)
+            return (i, j)
+        end
+        
+        (di, dj) = NEIGHBORS[rand(rng, 1:4)]
+        i += di
+        j += dj
+
+        # periodic boundary for left and right
+        j = (j - 1) % grid_len + 1
+
+        # absorbing boundary for top and bottom
+        if i < 1 || i > grid_len
+            return nothing
+        end
+    end
+    return nothing
+end
+
+
+function dla_simulation()
+    #= Monte Carlo simulation for DLA on 2D grid.
+    - origin in the middle of the bottom boundary
+    - sends out random walkers until max mass is reached =#
+
+    rng = MersenneTwister(seed)
+    origin = (grid_len, div(grid_len, 2))
+    cluster = Set{Tuple{Int, Int}}((origin,))
+    neighborhood = construct_neighborhood(cluster)
+
+    while length(cluster) < max_mass
+        attachment = release_random_walker(rng, neighborhood)
+        if attachment !== nothing
+            push!(cluster, attachment)
+            update_neighborhood(neighborhood, attachment, cluster)
+        end
+    end
+    return cluster
+end
+
+
+function plot_cluster(cluster)
+    #= Plots the cluster with origin at bottom
+    - swaps x and y 
+    - inverts y-axis =#
+    
+    x = [i for (i, _) in cluster]
+    y = [j for (_, j) in cluster]
+    
+    # x and y swaped
+    scatter(y, x, marker=:circle, color=:blue, size=(600, 600), axis=false, legend=false)
+    
+    # Invert y-axis 
+    plot!(yflip=true)
+    
+    # Make dots evenly spaced
+    plot!(aspect_ratio=1)
+end
+
+
+cluster = dla_simulation()
+plot_cluster(cluster)
