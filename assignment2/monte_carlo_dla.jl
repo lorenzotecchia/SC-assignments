cb2075917eb7612a813da8ec7fd4dd373e82e598
zzzimtzoe <zimtzoezottelzeo@pm.me>
2026-03-05 01:49:09 +0100

    added sticking probability

--- assignment2/monte_carlo_dla.jl
+++ assignment2/monte_carlo_dla.jl
@@ -103,6 +103,48 @@ function release_random_walker(rng, neighborhood)
 end
 
 
+function release_random_walker_ps(rng, neighborhood, ps, cluster)
+    #= Single random walker performs random walk with sticking probability ps.
+    - starts on launching point in top boundary
+    - added to cluster if reaches neighborhood with probability ps--> new walker
+    - cannot walk through cluster sites
+    - if random walker goes beyond the top and bottom boundary, it is removed --> new walker
+    - if random walker reaches left or right boundary, it keeps going on the other side (periodic boundary)
+    - if max steps reached, give up walker --> new walker =#
+
+    i, j = launch_point(rng)
+
+    for _ in 1:max_steps
+        # check if walker is on site in neighborhood
+        if in((i, j), neighborhood)
+            # attach to cluster with probability ps
+            if rand(rng) < ps
+                return (i, j)
+            end
+        end
+        
+        (di, dj) = NEIGHBORS[rand(rng, 1:4)]
+        step_i = i + di
+        step_j = j + dj
+
+        # walker cannot walk through cluster
+        if in((step_i, step_j), cluster)
+            continue
+        end
+        i, j = step_i, step_j
+
+        # periodic boundary for left and right
+        j = (j - 1) % grid_len + 1
+
+        # absorbing boundary for top and bottom
+        if i < 1 || i > grid_len
+            return nothing
+        end
+    end
+    return nothing
+end
+
+
 function dla_simulation()
     #= Monte Carlo simulation for DLA on 2D grid.
     - origin in the middle of the bottom boundary
@@ -124,11 +166,32 @@ function dla_simulation()
 end
 
 
+function dla_simulation_ps(ps)
+    #= Monte Carlo simulation for DLA on 2D grid with sticking probability ps.
+    - origin in the middle of the bottom boundary
+    - sends out random walkers until max mass is reached =#
+
+    rng = MersenneTwister(seed)
+    origin = (grid_len, div(grid_len, 2))
+    cluster = Set{Tuple{Int, Int}}((origin,))
+    neighborhood = construct_neighborhood(cluster)
+
+    while length(cluster) < max_mass
+        attachment = release_random_walker_ps(rng, neighborhood, ps, cluster)
+        if attachment !== nothing
+            push!(cluster, attachment)
+            update_neighborhood(neighborhood, attachment, cluster)
+        end
+    end
+    return cluster
+end
+
+
 function plot_cluster(cluster)
     #= Plots the cluster with origin at bottom
     - swaps x and y 
     - inverts y-axis =#
-    
+
     x = [i for (i, _) in cluster]
     y = [j for (_, j) in cluster]
     
@@ -143,5 +206,32 @@ function plot_cluster(cluster)
 end
 
 
-cluster = dla_simulation()
-plot_cluster(cluster)
+function plot_clusters_ps(probabilities)
+    pl = plot(layout = (1,3), size=(1200, 900))
+    for (i, ps) in enumerate(probabilities)
+        cluster = dla_simulation_ps(ps)
+        
+        x = [i for (i, _) in cluster]
+        y = [j for (_, j) in cluster]
+        
+        # plot in the i-th subplot
+        scatter!(pl[i], y, x, marker=:circle, color=:blue, axis=false, legend=false)
+        
+        # invert the y-axis for the plot to have the origin at the bottom
+        plot!(pl[i], yflip=true, title="Sticking Probability = $ps")
+        
+        # ensure the dots are evenly spaced (aspect ratio)
+        plot!(pl[i], aspect_ratio=1)
+
+        #title!(pl[i], "Sticking Probability = $ps")
+    end
+    display(pl)
+end
+
+
+#cluster = dla_simulation()
+#plot_cluster(cluster)
+
+# plot clusters with different ps
+probabilities = [0.1, 0.5, 1.0]
+plot_clusters_ps(probabilities)
